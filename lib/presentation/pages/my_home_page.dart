ce98c7830cf8ae7a714816bae3934aa0529fd54d
atketan <mr.ketanbhavsar@gmail.com>
2021-01-08 19:37:45 +0530

    added Subtract, Multiply and Division features/events

--- lib/presentation/pages/my_home_page.dart
+++ lib/presentation/pages/my_home_page.dart
@@ -37,41 +37,111 @@ class _MyHomePageState extends State<MyHomePage> {
                 BlocBuilder(
                   cubit: _counterBloc,
                   builder: (context, state) {
-                    String text = "Counter value is ${state.result}";
-                    return Text(text);
+                    return RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Counter value is\n',
+                          ),
+                          TextSpan(
+                            text: "${state.result}",
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
                   },
                 ),
-                new TextField(
-                  controller: _numberController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: 'Enter a number'),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    _counterBloc.add(
-                        CounterEvent.add(num.parse(_numberController.text)));
-                  },
-                  child: Text(
-                    'Add',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
+                /*SizedBox(
+                  height: 30.0,
+                ),*/
+                /*Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                  child: Divider(
+                    color: Colors.blueGrey,
+                  ),
+                ),*/
+                Container(
+                  margin: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 10.0),
+                  child: ListTile(
+                    title: new TextField(
+                      controller: _numberController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'Enter a number'),
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
                     ),
                   ),
                 ),
-                RaisedButton(
-                  onPressed: () {
-                    _counterBloc.add(
-                        CounterEvent.add(num.parse(_numberController.text)));
-                  },
-                  child: Text(
-                    'Add',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 200,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _counterBloc.add(
+                          CounterEvent.add(num.parse(_numberController.text)));
+                    },
+                    child: Text(
+                      'Add',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 200,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _counterBloc.add(CounterEvent.subtract(
+                          num.parse(_numberController.text)));
+                    },
+                    child: Text(
+                      'Subtract',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 200,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _counterBloc.add(CounterEvent.multiply(
+                          num.parse(_numberController.text)));
+                    },
+                    child: Text(
+                      'Multiply',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 200,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _counterBloc.add(
+                          DivisionEvent(num.parse(_numberController.text)));
+                    },
+                    child: Text(
+                      'Divide',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
